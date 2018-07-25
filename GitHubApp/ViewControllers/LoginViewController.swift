e0281c8f04cd4f080fe9f4a687afb00a9feeaed2
Vladyslav Filipov <38040492+VladyslavFilipov@users.noreply.github.com>
2018-07-25 10:08:56 +0300

    initial commit

---
+++ GitHubApp/ViewControllers/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  GitHubApp
+//
+//  Created by Vladyslav Filipov on 25.07.2018.
+//  Copyright © 2018 Vladislav Filipov. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+
+}
+
